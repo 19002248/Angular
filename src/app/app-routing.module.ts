3de7b407835a9d05ab990ae81f3fd0c6c456d831
19002248 <l.gis.cas@gmail.com>
2022-09-30 13:34:51 -0500

    práctica cinema, falta pulir el aspecto del html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { OperacionComponent } from './calculos/operacion/operacion.component';
 import { TablaComponent } from './calculos/tabla/tabla.component';
+import { CinemaComponent } from './calculos/cinema/cinema.component';
 import { ProductListComponent } from './product/product-list/product-list.component';
 
 const routes: Routes = [
   {path:'', component: ProductListComponent},
   {path:'suma', component: OperacionComponent},
-  {path:'tablaa', component: TablaComponent}
+  {path:'tablaa', component: TablaComponent},
+  {path:'cinepolis', component: CinemaComponent}
 ];
 
 @NgModule({
--- src/app/calculos/cinema/cinema.component.ts
+++ src/app/calculos/cinema/cinema.component.ts
@@ -6,10 +6,67 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cinema.component.css']
 })
 export class CinemaComponent implements OnInit {
+  //Cantidades ingresadas
+  nombree:string="";
+  compradores:number=0; //numero de compradores
+  tarjetaCine:string=""; //si se tiene o no la tarjeta especial 
+  ciboletas:number=0; //cant ingresada de boletas
 
-  constructor() { }
+  //Variables para almacenar
+  comprador:string="";
+  pagar:number=0; //aquí almaccenamos el costo final final
+  cbmax:number=0; //aquí almacenamos la multiplicación de la cant de compradores 
+                  //por el numero máximo de boletas, para evaluar que no compre más 
+                  //por persona
+  pagardesc:number=0;
+  mensajeerror:string="Supera las 7 boletas por persona, ingrese un número menor";
+  /* 
+  Condiciones:
+   2 boletas no hay descuento
+   de 3 a 5 = 10% de descuento
+   más de 5 boletas = 15% de descuento
 
+   además, si la compra es con la tarjeta del cine = 10% descuento 
+   o 10% adicional 
+  */
   ngOnInit(): void {
   }
-
+  
+  totalPago(){
+    this.pasarNombre();
+    this.cbmax=this.compradores*7;
+    if(this.ciboletas<= this.cbmax){
+      if (this.tarjetaCine=="s")/*Si hay tarjeta o nel*/{
+        if (this.ciboletas<=2) {
+          this.pagardesc=(this.ciboletas*12)
+          this.pagar=this.pagardesc-(this.pagardesc*.10);
+        }
+        if (this.ciboletas>=3&&this.ciboletas<=5) {
+          this.pagardesc=((this.ciboletas*12)-((this.ciboletas*12)*.10));
+          this.pagar=this.pagardesc-(this.pagardesc*.10);
+        }
+        if (this.ciboletas>5) {
+          this.pagardesc=((this.ciboletas*12)-((this.ciboletas*12)*.15));
+          this.pagar=this.pagardesc-(this.pagardesc*.10)
+        }
+      }
+      if(this.tarjetaCine=="n"){  
+        if (this.ciboletas<=2) {
+          this.pagar=(this.ciboletas*12)
+        }
+        if (this.ciboletas>=3&&this.ciboletas<=5) {
+          this.pagar=((this.ciboletas*12)-((this.ciboletas*12)*.10));
+        }
+        if (this.ciboletas>5) {
+          this.pagar=((this.ciboletas*12)-((this.ciboletas*12)*.15));
+        }
+      }
+    }
+    else{
+      this.mensajeerror;
+    }
+  }
+  pasarNombre(){
+    this.comprador=this.nombree;
+  }
 }
